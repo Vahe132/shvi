3a5bb30384dd2556f23c3f92edbd7ce7bc75ee13
darthspectrum <muradianator@gmail.com>
2025-05-11 01:48:21 +0400

    :construction: setup for step 03

--- sintez.js
+++ sintez.js
@@ -1,4 +1,4 @@
-export { encodeWAV, generatePCM, tokenize, typeify };
+export { encodeWAV, evaluate, generatePCM, tokenize, typeify };
 
 // sample[n]= A ⋅ sin(2 * π * f * (n / R)​)
 
@@ -67,36 +67,25 @@ async function encodeWAV(
 const atom = (name) => Symbol.for(name);
 
 const typeify = (token) => {
-  const number = Number.parseFloat(token, 10);
-  return Number.isNaN(number) ? atom(token) : number;
+  throw new Error("Not implemented");
 };
 
 const tokenize = (input) => {
-if(input == []){
-  return []
-}
-
-  const splitt = input.split(" ");
-  const tokens = [];
-  for (let y = 0; y < splitt.length; y++) {
-    if (isNaN(Number(splitt[y]))) {
-      tokens.push(atom(splitt[y]));
-    }
-    
-    else {
-      tokens.push(Number(splitt[y]));
-    }
+  if (input == []) {
+    return [];
   }
 
-  return tokens;
+  const loop = (
+    progressiveScope,
+    [graphemeAtHand, ...restOfGraphemes],
+    tokenSoFar = "",
+  ) => {
+    throw new Error("Not implemented");
+  };
+
+  return loop([[]], graphemes);
 };
 
-const run = (
-  program,
-  definitions = [],
-) => {
-  const tokens = tokenize(program);
-  const value = evaluate(tokens, definitions);
-
-  return value;
+const evaluate = (expression) => {
+  throw new Error("Not implemented");
 };
